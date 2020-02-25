135185a0a0732505ebf3845b4468dfe9ddc0a45e
Jatin Jain Saraf <jatinj2327@gmail.com>
2020-02-25 19:42:42 +0530

    a

--- js-exercise-3/js/signin.js
+++
@@ -1,4 +0,0 @@
-let name = document.querySelector('#name')
-let email = document.querySelector('#email')
-let password = document.querySelector('#password')
-let confirmPassword = document.querySelector('#confirmPassword')
